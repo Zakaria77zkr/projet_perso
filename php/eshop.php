b62c9878974f70700011bb03f7b440363e31d087
Zakaria77zkr <118564853+Zakaria77zkr@users.noreply.github.com>
2024-01-25 15:54:17 +0100

    Add files via upload

---
+++ php/eshop.php
@@ -0,0 +1,48 @@
+<?php
+include("includex/header.php");
+?>
+<?php
+include("includex/connexion.php");
+?>
+<?php
+include("includex/inscription.php");
+?>
+
+    <main class="eshop_main">
+        <div class="eshop_container">
+            <div class="eshop_nouv">
+                <img src="assets/ressources/eshop/nouv.jpg" alt="">
+                <h2 class="eshop_main_titre">NOUVELLE ELEGANCE</h2>
+                <a class="eshop_main_btn" href="elegances.php">DECOUVRIR LA COLLECTION</a>
+            </div>
+        
+
+            <div class="eshop_main_centre">
+                <div class="eshop_blouson">
+                    <h2 class="eshop_main_titre">VESTES ET BLOUSONS</h2>
+                    <img src="assets/ressources/eshop/blousons.jpg" alt="">
+                    
+                    <a class="eshop_main_btn" href="blousons.php">DECOUVRIR</a>
+                </div>
+                <div class="eshop_jeans">
+                    <h2 class="eshop_main_titre">NOS JEANS</h2>
+                    <img src="assets/ressources/eshop/jeans.jpg" alt="">
+                    
+                    <a class="eshop_main_btn" href="jeans.php">DECOUVRIR</a>
+                </div>
+            </div>    
+
+
+            <div class="eshop_perm">
+                <img src="assets/ressources/eshop/perm.jpg" alt="">
+                <h2 class="eshop_main_titre">LES PERMANENTS</h2>
+                <a class="eshop_main_btn" href="permanents.php">TROUVEZ VOS INDISPENSABLES</a>
+            </div>
+        </div>
+    </main>
+    <?php
+include("includex/footer.php");
+?>
+
+
+
