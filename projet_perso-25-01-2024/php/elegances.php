ff4a82c97945b402410f820abe8e6fc1e88257b7
Zakaria77zkr <118564853+Zakaria77zkr@users.noreply.github.com>
2024-01-25 22:06:37 +0100

    Add files via upload

---
+++ projet_perso-25-01-2024/php/elegances.php
@@ -0,0 +1,153 @@
+<?php
+include("includex/header.php")
+?>
+<?php
+include("includex/connexion.php");
+?>
+<?php
+include("includex/inscription.php");
+?>
+
+
+<main class="articles_main">
+    <h1>Nouvelles Elégances</h1>
+        <section class="articles_section">
+            <ul class="list_articles">
+            
+            
+            <div class="bloc_article">
+                <li class="element_article">
+                    <a href="index.php">
+                        <img src="assets/ressources/caroussel_accueil/1.webp" alt="">
+                        <div class="bloc_description">
+                            <div class="bloc_description_gauche">
+                                <p>Veste Oropa marine</p>
+                                <p>Laine et isolant Climashield</p>
+                            </div>
+                            <div class="bloc_description_droit">
+                                <p>290,00 €</p>
+                                <p>2 Couleurs</p>
+                            <div>
+                        </div>
+                    </a>
+                </li>
+                <li class="element_article">
+                    <img src="assets/ressources/caroussel_accueil/1.webp" alt="">
+                    <div class="bloc_description">
+                        <div class="bloc_description_gauche">
+                            <p>Veste Oropa marine</p>
+                            <p>Laine et isolant Climashield</p>
+                        </div>
+                        <div class="bloc_description_droit">
+                            <p>290,00 €</p>
+                            <p>2 Couleurs</p>
+                        <div>
+                    </div>
+                </li>
+                <li class="element_article">
+                    <img src="assets/ressources/caroussel_accueil/1.webp" alt="">
+                    <div class="bloc_description">
+                        <div class="bloc_description_gauche">
+                            <p>Veste Oropa marine</p>
+                            <p>Laine et isolant Climashield</p>
+                        </div>
+                        <div class="bloc_description_droit">
+                            <p>290,00 €</p>
+                            <p>2 Couleurs</p>
+                        <div>
+                    </div>
+                </li>
+            </div>
+            <div class="bloc_article">
+                <li class="element_article">
+                    <img src="assets/ressources/caroussel_accueil/1.webp" alt="">
+                    <div class="bloc_description">
+                        <div class="bloc_description_gauche">
+                            <p>Veste Oropa marine</p>
+                            <p>Laine et isolant Climashield</p>
+                        </div>
+                        <div class="bloc_description_droit">
+                            <p>290,00 €</p>
+                            <p>2 Couleurs</p>
+                        <div>
+                    </div>
+                </li>
+                <li class="element_article">
+                    <img src="assets/ressources/caroussel_accueil/1.webp" alt="">
+                    <div class="bloc_description">
+                        <div class="bloc_description_gauche">
+                            <p>Veste Oropa marine</p>
+                            <p>Laine et isolant Climashield</p>
+                        </div>
+                        <div class="bloc_description_droit">
+                            <p>290,00 €</p>
+                            <p>2 Couleurs</p>
+                        <div>
+                    </div>
+                </li>
+                <li class="element_article">
+                    <img src="assets/ressources/caroussel_accueil/1.webp" alt="">
+                    <div class="bloc_description">
+                        <div class="bloc_description_gauche">
+                            <p>Veste Oropa marine</p>
+                            <p>Laine et isolant Climashield</p>
+                        </div>
+                        <div class="bloc_description_droit">
+                            <p>290,00 €</p>
+                            <p>2 Couleurs</p>
+                        <div>
+                    </div>
+                </li>
+            </div>
+            <div class="bloc_article">
+                <li class="element_article">
+                    <img src="assets/ressources/caroussel_accueil/1.webp" alt="">
+                    <div class="bloc_description">
+                        <div class="bloc_description_gauche">
+                            <p>Veste Oropa marine</p>
+                            <p>Laine et isolant Climashield</p>
+                        </div>
+                        <div class="bloc_description_droit">
+                            <p>290,00 €</p>
+                            <p>2 Couleurs</p>
+                        <div>
+                    </div>
+                </li>
+                <li class="element_article">
+                    <img src="assets/ressources/caroussel_accueil/1.webp" alt="">
+                    <div class="bloc_description">
+                        <div class="bloc_description_gauche">
+                            <p>Veste Oropa marine</p>
+                            <p>Laine et isolant Climashield</p>
+                        </div>
+                        <div class="bloc_description_droit">
+                            <p>290,00 €</p>
+                            <p>2 Couleurs</p>
+                        <div>
+                    </div>
+                </li>
+                <li class="element_article">
+                    <img src="assets/ressources/caroussel_accueil/1.webp" alt="">
+                    <div class="bloc_description">
+                        <div class="bloc_description_gauche">
+                            <p>Veste Oropa marine</p>
+                            <p>Laine et isolant Climashield</p>
+                        </div>
+                        <div class="bloc_description_droit">
+                            <p>290,00 €</p>
+                            <p>2 Couleurs</p>
+                        <div>
+                    </div>
+                </li>
+            </div>
+            </ul>
+</section>
+        <p>LA NOUVELLE ÉLÉGANCE
+Pour nous, la nouvelle élégance est celle qui nous distingue sans donner l'impression de trop en faire, grice une étoffe au
+tombé chic mais la texture décontractée, C'est celle qui nous met en valeur sans qu'on se sente guindé dans nos vétements,
+grace la précision de leurs coupes. C'est celle qui ne nous empéche pu de nous amuser avec notre style, grsce des formes
+et des couleurs originales. A travers cette nouvelle collection, nous tenions explorer ce nombreuses possibilités avec vous.</p>
+    </main>
+<?php
+include("includex/footer.php")
+?>
