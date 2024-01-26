1e0bdb9cb434c607905537c5d1c0847f7911684a
Zakaria77zkr <118564853+Zakaria77zkr@users.noreply.github.com>
2024-01-26 15:44:53 +0100

    Add files via upload

---
+++ projet_perso-25-01-2024/projet_perso-25-01-2024/php/assets/js/script.js
@@ -0,0 +1,85 @@
+// RECHERCHER
+
+let rechercher = document.querySelector(".rechercher");
+let loupe =  document.querySelector(".loupe");
+loupe.addEventListener("click" , function(){
+    if(rechercher.classList.contains("hidden")){
+    rechercher.classList.remove("hidden");
+    }else{
+        rechercher.classList.add("hidden");
+    }
+})
+
+//  INSCRIPTION/CONNEXION
+let inscricontainer = document.querySelector(".inscription_container");
+let cocontainer = document.querySelector(".connexion_container");
+let cobutton = document.querySelector(".connexion_button");
+let inscrilien = document.querySelector(".inscription_lien");
+let colien = document.querySelector(".connexion_lien");
+let closeco = document.querySelector(".close_co");
+let closeinscri =document.querySelector(".close_inscri");
+let overlayco = document.querySelector(".overlay_co");
+let overlayinscri =document.querySelector(".overlay_inscri");
+
+
+cobutton.addEventListener("click" , function(){
+    cocontainer.classList.remove("hidden");
+})
+
+
+closeco.addEventListener("click" , function(){
+    cocontainer.classList.add("hidden");
+})
+overlayco.addEventListener("click" , function(){
+    cocontainer.classList.add("hidden");
+})
+document.addEventListener("keydown" , function(e){
+    if(e.key==="Escape" && !cocontainer.classList.contains("hidden")){
+        cocontainer.classList.add("hidden");}
+    
+})
+
+
+
+
+closeinscri.addEventListener("click" , function(){
+    inscricontainer.classList.add("hidden");
+})
+overlayinscri.addEventListener("click" , function(){
+    inscricontainer.classList.add("hidden");
+})
+document.addEventListener("keydown" , function(e){
+    if(e.key==="Escape" && !inscricontainer.classList.contains("hidden")){
+        inscricontainer.classList.add("hidden");}
+})
+
+inscrilien.addEventListener("click" , function(){
+    inscricontainer.classList.remove("hidden");
+    cocontainer.classList.add("hidden");
+})
+colien.addEventListener("click" , function(){
+    cocontainer.classList.remove("hidden");
+    inscricontainer.classList.add("hidden");
+})
+
+
+
+// SLIDER PRINCIPAL (1) ACCUEIL
+
+const slider = ["./assets/ressources/accueil/accueil_boutiques.jpg", "./assets/ressources/accueil/accueil_concept.webp","./assets/ressources/accueil/accueil_eshop.webp"];
+let num = 0;
+async function changeSlide(sens){
+    num=num + sens;
+    if(num < 0) {
+        num = slider.length -1;
+    } else
+     if (num > slider.length -1) {
+        num = 0;
+     }
+
+document.getElementById("change").src=slider[num]
+
+}
+                             
+
+
