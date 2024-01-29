349ef5fec2d390441f88b618f72beb41960ad73e
Zakaria77zkr <118564853+Zakaria77zkr@users.noreply.github.com>
2024-01-29 15:57:21 +0100

    Add files via upload

---
+++ php/jeans.php
@@ -0,0 +1,168 @@
+<?php
+include("includex/header.php")
+?>
+<?php
+include("includex/connexion.php");
+?>
+<?php
+include("includex/inscription.php");
+?>
+
+<main class="article_main">
+    <h1 class="titre_article">NOS JEANS</h1>
+        <section class="article_section">
+            <ul class="list_article">
+            
+            
+            <div class="bloc_article">
+                    <li class="element_article">
+                        <a href="index.php">
+                            <img src="assets/ressources/caroussel_accueil/1.webp" alt="">
+                            <div class="bloc_description">
+                                <div class="bloc_description_gauche">
+                                    <p>Veste Oropa marine</p>
+                                    <p>Laine et isolant Climashield</p>
+                                </div>
+                                <div class="bloc_description_droit">
+                                    <p>290,00 €</p>
+                                    <p>2 Couleurs</p>
+                                <div>
+                            </div>
+                        </a>
+                    </li>
+                    <li class="element_article">
+                        <a href="index.php">
+                            <img src="assets/ressources/caroussel_accueil/1.webp" alt="">
+                            <div class="bloc_description">
+                                <div class="bloc_description_gauche">
+                                    <p>Veste Oropa marine</p>
+                                    <p>Laine et isolant Climashield</p>
+                                </div>
+                                <div class="bloc_description_droit">
+                                    <p>290,00 €</p>
+                                    <p>2 Couleurs</p>
+                                <div>
+                            </div>
+                        </a>
+                    </li>
+                    <li class="element_article">
+                        <a href="index.php">
+                            <img src="assets/ressources/caroussel_accueil/1.webp" alt="">
+                            <div class="bloc_description">
+                                <div class="bloc_description_gauche">
+                                    <p>Veste Oropa marine</p>
+                                    <p>Laine et isolant Climashield</p>
+                                </div>
+                                <div class="bloc_description_droit">
+                                    <p>290,00 €</p>
+                                    <p>2 Couleurs</p>
+                                <div>
+                            </div>
+                        </a>
+                    </li>
+                </div>
+                <div class="bloc_article">
+                    <li class="element_article">
+                        <a href="index.php">
+                            <img src="assets/ressources/caroussel_accueil/1.webp" alt="">
+                            <div class="bloc_description">
+                                <div class="bloc_description_gauche">
+                                    <p>Veste Oropa marine</p>
+                                    <p>Laine et isolant Climashield</p>
+                                </div>
+                                <div class="bloc_description_droit">
+                                    <p>290,00 €</p>
+                                    <p>2 Couleurs</p>
+                                <div>
+                            </div>
+                        </a>
+                    </li>
+                    <li class="element_article">
+                        <a href="index.php">
+                            <img src="assets/ressources/caroussel_accueil/1.webp" alt="">
+                            <div class="bloc_description">
+                                <div class="bloc_description_gauche">
+                                    <p>Veste Oropa marine</p>
+                                    <p>Laine et isolant Climashield</p>
+                                </div>
+                                <div class="bloc_description_droit">
+                                    <p>290,00 €</p>
+                                    <p>2 Couleurs</p>
+                                <div>
+                            </div>
+                        </a>
+                    </li>
+                    <li class="element_article">
+                        <a href="index.php">
+                            <img src="assets/ressources/caroussel_accueil/1.webp" alt="">
+                            <div class="bloc_description">
+                                <div class="bloc_description_gauche">
+                                    <p>Veste Oropa marine</p>
+                                    <p>Laine et isolant Climashield</p>
+                                </div>
+                                <div class="bloc_description_droit">
+                                    <p>290,00 €</p>
+                                    <p>2 Couleurs</p>
+                                <div>
+                            </div>
+                        </a>
+                    </li>
+                </div>
+                <div class="bloc_article">
+                    <li class="element_article">
+                        <a href="index.php">
+                            <img src="assets/ressources/caroussel_accueil/1.webp" alt="">
+                            <div class="bloc_description">
+                                <div class="bloc_description_gauche">
+                                    <p>Veste Oropa marine</p>
+                                    <p>Laine et isolant Climashield</p>
+                                </div>
+                                <div class="bloc_description_droit">
+                                    <p>290,00 €</p>
+                                    <p>2 Couleurs</p>
+                                <div>
+                            </div>
+                        </a>
+                    </li>
+                    <li class="element_article">
+                        <a href="index.php">
+                            <img src="assets/ressources/caroussel_accueil/1.webp" alt="">
+                            <div class="bloc_description">
+                                <div class="bloc_description_gauche">
+                                    <p>Veste Oropa marine</p>
+                                    <p>Laine et isolant Climashield</p>
+                                </div>
+                                <div class="bloc_description_droit">
+                                    <p>290,00 €</p>
+                                    <p>2 Couleurs</p>
+                                <div>
+                            </div>
+                        </a>
+                    </li>
+                    <li class="element_article">
+                        <a href="index.php">
+                            <img src="assets/ressources/caroussel_accueil/1.webp" alt="">
+                            <div class="bloc_description">
+                                <div class="bloc_description_gauche">
+                                    <p>Veste Oropa marine</p>
+                                    <p>Laine et isolant Climashield</p>
+                                </div>
+                                <div class="bloc_description_droit">
+                                    <p>290,00 €</p>
+                                    <p>2 Couleurs</p>
+                                <div>
+                            </div>
+                        </a>
+                    </li>
+                </div>
+            </ul>
+</section>
+        <p>Ici, Vous êtes sur un lieu de partage. Le partage de notre passion pour les <span class="bold">toiles brutes japonaises aux cotons qui se patinent,
+l'indigo profond des toiles italiennes,</span> la beauté des délavages écologiques, le charme des liserés selvedge, l'esprit vintage d'une
+toile bleach...
+Car <span class="bold">le denim authentique et nous, c'est une longue histoire d'amour</span> : notre toute <b>première pièce</b> était un jean en 2012... et
+certains d'entre vous le portent encore !</p>
+    </main>
+<?php
+include("includex/footer.php")
+?>
