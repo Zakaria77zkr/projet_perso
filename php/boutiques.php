2971386dbd75122d32209c78fd43f546582324cf
Zakaria77zkr <118564853+Zakaria77zkr@users.noreply.github.com>
2024-01-30 21:03:31 +0100

    Add files via upload

---
+++ php/boutiques.php
@@ -0,0 +1,61 @@
+<?php
+include("includex/header.php");
+?>
+    <?php
+include("includex/connexion.php");
+?>
+<?php
+include("includex/inscription.php");
+?>
+    <main>
+        <h1>NOS BOUTIQUES</h1>
+        <section class="boutiques_section1">
+        <div class="bloc_style white">
+            <img src="./assets/ressources/boutiques/boutique_elegance.jpg" alt="" >
+            </div>
+            <div class="bloc_style beige">
+                <div class="vide"></div>
+                <div class="blue_para"><p>De vrais lieux de vie
+                    Notre marque est née sur le digital et tout en restant fidèles à nos valeurs, on a voulu aller encore plus loin : vous rencontrer, vous conseiller, échanger en direct... Voilà pourquoi nous avons posé nos valises dans plusieurs villes de France.
+                    À travers des services uniques pensés pour vous, notre mission reste la même : vous guider pour trouver votre propre style et surtout, pour que vous vous sentiez bien dans vos vêtements.
+                    Passez nous voir, nos conseillers vous attendent !</p>
+                </div>
+        </div>
+        </section>
+
+
+
+        <section class="boutiques_section2">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1855.5276068370804!2d2.2947360975376982!3d48.87513556053303!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66fec70fb1d8f%3A0xd9b5676e112e643d!2sArc%20de%20Triomphe!5e0!3m2!1sfr!2sfr!4v1697303692754!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </section>
+
+        <section class="boutiques_section3">
+            <div class="boutiques_rdv">
+                <div class="boutiques_rdv_haut">
+                    <img src="./assets/ressources/boutiques/2.webp" alt="">
+                    <img src="./assets/ressources/boutiques/1.webp" alt="">
+                </div>
+                <div class="boutiques_rdv_centre">
+                    <h2>RENDEZ-VOUS PERSONNALISE</h2>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et aut nostrum totam, iusto est ipsam fuga amet optio impedit nemo! Voluptates provident libero unde. Vitae cum ipsa amet fuga minima?</p>
+                    <a href="#">Prendre rendez-vous</a>
+                </div>
+                <div class="bloc_style white">
+        <img src="./assets/ressources/boutiques/boutiquefin.webp" alt="">
+            </div>
+            <div class="bloc_style blue">
+                <div class="vide"></div>
+                <div class="blue_para"><p>De vrais lieux de vie
+                    Notre marque est née sur le digital et tout en restant fidèles à nos valeurs, on a voulu aller encore plus loin : vous rencontrer, vous conseiller, échanger en direct... Voilà pourquoi nous avons posé nos valises dans plusieurs villes de France.
+                    À travers des services uniques pensés pour vous, notre mission reste la même : vous guider pour trouver votre propre style et surtout, pour que vous vous sentiez bien dans vos vêtements.
+                    Passez nous voir, nos conseillers vous attendent !</p>
+                </div>
+        </div>
+        </section>
+
+       
+    </main>
+    <?php
+include("includex/footer.php");
+?>
