1708690073d52dcdfdfe29f0d0490f7abc825b57
Zakaria77zkr <118564853+Zakaria77zkr@users.noreply.github.com>
2024-02-01 15:48:40 +0100

    Add files via upload

---
+++ concept.php
@@ -0,0 +1,83 @@
+<?php
+include("includex/header.php");
+?>
+<?php
+include("includex/connexion.php");
+?>
+<?php
+include("includex/inscription.php");
+?>
+
+<main class="concept_main">
+    <h1 class="concept_titre">QUI SOMMES NOUS ?</h1>
+
+        <section class="concept_section1">
+            <div class="bloc_style white">
+                <img src="./assets/ressources/concept/1.webp" alt="" >
+            </div>
+            <div class="bloc_style beige">
+                <div class="vide"></div>
+                <div class="blue_para"><p>De vrais lieux de vie
+                    Notre marque est née sur le digital et tout en restant fidèles à nos valeurs, on a voulu aller encore plus loin : vous rencontrer, vous conseiller, échanger en direct... Voilà pourquoi nous avons posé nos valises dans plusieurs villes de France.
+                    À travers des services uniques pensés pour vous, notre mission reste la même : vous guider pour trouver votre propre style et surtout, pour que vous vous sentiez bien dans vos vêtements.
+                    Passez nous voir, nos conseillers vous attendent !</p>
+                </div>
+        </div>
+        </section>
+
+
+
+
+
+
+        <section class="concept_section2">
+            <div class="bloc_media">
+                <img src="assets\ressources\concept\moitie1.webp" alt="">
+                <div class="media">Un Média écrit par des passionné(e)s qui vous aide à tomber amoureux des belles matières, à apprécier les détails que seuls les initiés remarquent. C’est aussi un carnet de voyage pour comprendre le style avant de trouver le sien. Oui, l’important ce n’est pas la destination qui compte, c’est le voyage.Nous écrivons des articles depuis 2007 par conviction sur des sujets qui nous passionnent et non par opportunisme. Et depuis 2011, nous avons créé une chaîne Youtube pour vous partager tous nos conseils et nos découvertes</div>
+            </div>
+            <div class="impact">
+                <h2>Pilier Impact</h2>
+                <p>Nous mettons en lumière des savoir-faire et des matières aussi exceptionnels que responsables pour vous proposer des vêtements beaux et fonctionnels. Acheter des vêtements BonneGueule, c’est contribuer à une mode masculine plus vertueuse. A rendre le monde meilleur.</p>
+            </div>
+        </section>
+        <section class="concept_section3">
+            <div class="bloc_pilier experience">
+                <img src="assets\ressources\concept\moitie2.webp" alt="">
+                <div class="pilier">
+                    <h2>PILIER EXPERIENCE</h2>
+                    <p>Aider, soutenir et accompagner, c’est notre raison d’être chez BonneGueule. Nous sommes le trait d’union entre nos clients et leur relation à leur vêtement. Nous essayons humblement de vous éclairer pour vous donner toutes les clés afin de vous aider à faire les bons choix. 1. Une ambition d’écrire les meilleurs articles du web et de vous offrir une expérience client juste, honnête et conviviale sur le web et en boutiques. 2. Vous donner de nouvelles perspectives en proposant aux hommes une manière de consommer en accord avec leurs valeurs. 3. Échanger quotidiennement avec vous en commentaires de nos articles et de nos vidéos Youtube ou encore sur Instagram et Discord.</p>
+                </div>
+            </div>
+            <div class="bloc_pilier communaute">
+                <img src="assets\ressources\concept\moitie3.webp" alt="">
+                <div class="pilier">
+                    <h2>PILIER COMMUNAUTE</h2>
+                    <p>Notre rôle, c’est de créer une réflexion qui appelle au changement entre les personnes et leur rapport aux vêtements. C’est une envie sincère de vous informer et de vous conseiller grâce à une expertise unique développée depuis plus de 15 ans. Nous voulons vous donner le pouvoir de consommer en conscience et développer une nouvelle sensibilité faisant évoluer votre mode de vie.</p>
+                </div>
+            </div>
+            <div class="bloc_pilier engagements">
+                <img src="assets\ressources\concept\moitie4.webp" alt="">
+                <div class="pilier">
+                    <h2>NOS ENGAGEMENTS</h2>
+                    <p>Nous sommes loin d’être parfaits mais nous ne voulons faire aucun compromis sur la durabilité des produits, l'impact environnemental de la matière et santé physique & mentale des artisans qui fabriquent nos vêtements. Nous privilégions les textures riches et les matières naturelles produites de manière éthique.</p>
+                </div>
+            </div>
+        </section>
+        <section class="concept_section4">
+        <div class="bloc_style white">
+        <img src="assets\ressources\concept\2.webp" alt="">
+            </div>
+            <div class="bloc_style blue">
+                <div class="vide"></div>
+                <div class="blue_para"><p>De vrais lieux de vie
+                    Notre marque est née sur le digital et tout en restant fidèles à nos valeurs, on a voulu aller encore plus loin : vous rencontrer, vous conseiller, échanger en direct... Voilà pourquoi nous avons posé nos valises dans plusieurs villes de France.
+                    À travers des services uniques pensés pour vous, notre mission reste la même : vous guider pour trouver votre propre style et surtout, pour que vous vous sentiez bien dans vos vêtements.
+                    Passez nous voir, nos conseillers vous attendent !</p>
+                </div>
+        </div>
+        </section>
+        
+</main>
+<?php
+include("includex/footer.php");
+?>
