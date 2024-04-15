5e5cbce1c25296365ff86241727a52be67eb0d28
Zakaria77zkr <118564853+Zakaria77zkr@users.noreply.github.com>
2024-04-15 23:51:39 +0200

    Add files via upload

---
+++ assets/js/fonction.js
@@ -0,0 +1,11 @@
+// let password = document.getElementById("password");
+// let confirmPassword = document.getElementById("confirm-password");
+// let connectError = document.querySelector(".connectError");
+
+// function confirm_pass() {
+
+//     if (password.value !== confirmPassword.value) {
+//         connectError.textContent = "Vos mots de passe ne sont pas identiques !";
+//         return false;  
+//     }       
+// }
